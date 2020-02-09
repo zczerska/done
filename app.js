66296ea5dc2f90c98b4442ece1e02baa81bb5d04
zczerska <zofia.hello@gmail.com>
2020-02-09 13:56:56 -0500

    trial run partial fail

--- app.js
+++ app.js
@@ -3,19 +3,30 @@ function onReady() {
    const NEW_TODO_TEXT = document.getElementById('newToDoText');
    const TODO_LIST = document.getElementById('toDoList');
 	
+	
 	ADD_TODO_FORM.addEventListener('submit',(event) =>{
 		event.preventDefault();
 		
 		let title = NEW_TODO_TEXT.value;
+		
+		
 		
 		let newLi = document.createElement('li'); //creates empty li
 		
 		let checkbox = document.createElement('input');
 		checkbox.type = "checkbox";
 		
+		//checkbox.className = 'mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect';
+        //componentHandler.upgradeElement(checkbox);
+		
 		let deleteBtn = document.createElement('button');
 		deleteBtn.textContent = "Delete";
 		
+		
+		deleteBtn.className = 'mdl-button mdl-js-button mdl-js-ripple-effect';
+        componentHandler.upgradeElement(deleteBtn);
+		deleteBtn.style.backgroundColor = "indigo";
+		deleteBtn.style.color = "white";
 		
 		deleteBtn.addEventListener('click', function(event){
 			TODO_LIST.removeChild(this.parentElement);
@@ -29,7 +40,10 @@ function onReady() {
 		
 		
 	});
+	
+	
  }; 
+
 
 
 window.onload = function() {
